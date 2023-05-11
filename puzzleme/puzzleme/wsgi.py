da8181fb3c427cee91ffcfa0d336a6a6c9d9cec5
Prarthan <prarthantalwar@gmail.com>
2023-05-12 04:25:22 +0530

    append sys.path

--- puzzleme/puzzleme/wsgi.py
+++ puzzleme/puzzleme/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+sys.path.append('C:/dev/django_projects/puzzleme')
+sys.path.append('C:/dev/django_projects/puzzleme/puzzleme')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzleme.settings')
 # os.environ["DJANGO_SETTINGS_MODULE"] = "puzzleme.settings"
