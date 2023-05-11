1fdcaa5e6c351b119971ff07e75e8eccbf4f114f
Prarthan <prarthantalwar@gmail.com>
2023-05-12 00:59:05 +0530

    Changed wsgi

--- puzzleme/puzzleme/wsgi.py
+++ puzzleme/puzzleme/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzleme.settings')
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "puzzleme.settings"
 application = Cling(get_wsgi_application())
