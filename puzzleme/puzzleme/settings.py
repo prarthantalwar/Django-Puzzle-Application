22d6d96c6d120fee2f10fd9999f6e6fb5a32e32a
Prarthan <prarthantalwar@gmail.com>
2023-05-14 11:57:59 +0530

    Removing corsheaders

--- puzzleme/puzzleme/settings.py
+++ puzzleme/puzzleme/settings.py
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app1',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
